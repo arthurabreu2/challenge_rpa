c70f41d7b93f7d2548df39e3aee3e66fbb89508c
Arthur Abreu <arthurabreu2000@gmail.com>
2024-07-30 14:17:42 -0300

    Update challenge

--- rpa_challenge.py
+++ rpa_challenge.py
@@ -55,9 +55,10 @@ class FreshNews:
         """
         try:
             options = Options()
-            options.headless = True  
+            options.add_argument("--headless")  
             options.add_argument("--no-sandbox")  
-            options.add_argument("--disable-dev-shm-usage") 
+            options.add_argument("--disable-dev-shm-usage")  
+
             driver = webdriver.Chrome(options=options)
             driver.get("https://www.latimes.com")
             return driver
